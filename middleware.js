2210b0b7754e3aa7f8a6f7a64198e3a913a26db5
FreakQnZ <anuraag.srivatsa123@gmail.com>
2024-01-27 12:11:26 +0530

    made routes public

--- middleware.js
+++ middleware.js
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ['/', '/about', '/contact' ],
+  publicRoutes: ['/', '/about', '/contact', '/LandingPage', '/StartupHomePage', '/StudentHomePage' ],
   // ignoredRoutes: ['/api/all', '/api/new', '/api/update', '/api/delete'],
   ignoredRoutes: ['/api/verify', 'api/new', 'api/newStartup', '/api/getAboutUs', '/api/getListing','/api/addLor','/api/addResume','/api/addPortfolio','/api/applyForListing','/api/getListingsForStartup', '/api/studentDetails']
 });
